bcae9cd70cf5bb9ec6c4c0a7e68d48a078f9157f
psznm <psznm@seznam.cz>
2022-06-16 00:41:54 +0200

    Better mod compatibility

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,20 +2,11 @@
 local eq1 = data.raw["movement-bonus-equipment"]["DoubleSpeedSpidertron_exoskeleton-equipment"]
 local eq2 = data.raw["generator-equipment"]["DoubleSpeedSpidertron_fusion-reactor-equipment"]
 
-if mods["bobvehicleequipment"] then
-    table.insert(eq1.categories, "vehicle-equipment")
-    table.insert(eq2.categories, "vehicle-equipment")
-end
-if mods["vtk-armor-plating"] then
-    table.insert(eq1.categories, "vtk-armor-plating")
-    table.insert(eq2.categories, "vtk-armor-plating")
-end
-if mods["Krastorio2"] then
-    table.insert(eq1.categories, "universal-equipment")
-    table.insert(eq1.categories, "vehicle-equipment")
-    table.insert(eq1.categories, "vehicle-motor")
-	
-    table.insert(eq2.categories, "universal-equipment")
-    table.insert(eq2.categories, "vehicle-equipment")
-    table.insert(eq2.categories, "vehicle-motor")
+for index, spider in pairs(data.raw["spider-vehicle"]) do
+    local gridName = spider.equipment_grid
+
+    for index, eqCategory in pairs(data.raw["equipment-grid"][gridName].equipment_categories) do
+        table.insert(eq1.categories, eqCategory)
+        table.insert(eq2.categories, eqCategory)
+    end
 end
