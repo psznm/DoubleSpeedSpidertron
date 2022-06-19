9c731215e8e8d4a79eb6dd875fc63558ac15fe46
psznm <psznm@seznam.cz>
2022-06-19 16:31:21 +0200

    Spidertron engineer loading crash fix

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -5,8 +5,11 @@ local eq2 = data.raw["generator-equipment"]["DoubleSpeedSpidertron_fusion-reacto
 for index, spider in pairs(data.raw["spider-vehicle"]) do
     local gridName = spider.equipment_grid
 
-    for index, eqCategory in pairs(data.raw["equipment-grid"][gridName].equipment_categories) do
-        table.insert(eq1.categories, eqCategory)
-        table.insert(eq2.categories, eqCategory)
+    local grid = data.raw["equipment-grid"][gridName]
+    if (grid ~= nil and grid.equipment_categories ~= nil ) then
+        for index, eqCategory in pairs(grid.equipment_categories) do
+            table.insert(eq1.categories, eqCategory)
+            table.insert(eq2.categories, eqCategory)
+        end
     end
 end
